973aeda3a34a98ea952cadec4378de496f6e35de
Romen Adama Caetano Ramirez <Romen_Adama_CR@Romens-MacBook-Air.local>
2023-03-12 02:31:40 +0000

    mensaje de commit

---
+++ script_git.sh
@@ -0,0 +1,10 @@
+//Conocer la rama
+git branch
+//Añadir los cambios
+git add .
+//Comentario
+git commit -m "mensaje de commit"
+//Cambios remotos y fusionarlos
+git pull origin main
+//Actualizacion del repositorio
+git push origin main
