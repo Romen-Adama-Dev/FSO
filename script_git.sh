381bac3babf4aeab446c59980a4c318fd4a4669e
Romen Adama Caetano Ramirez <Romen_Adama_CR@Romens-MacBook-Air.local>
2023-03-13 21:19:21 +0000

    Desarrollo de la primera practica de FSO al 100%, creacion de la biblioteca y un directorio especifico para los archivos definidos como .o

--- script_git.sh
+++ script_git.sh
@@ -3,7 +3,7 @@ git branch
 //Añadir los cambios
 git add .
 //Comentario
-git commit -m "Desarrollo de la primera practica de FSO al 100%, añadidos los comentarios al codigo para explicar su funcionamiento"
+git commit -m "Desarrollo de la primera practica de FSO al 100%, creacion de la biblioteca y un directorio especifico para los archivos definidos como .o"
 //Cambios remotos y fusionarlos
 git pull origin main
 //Actualizacion del repositorio
