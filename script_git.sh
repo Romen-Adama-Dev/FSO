63acbb2df19d825c80184ff4d7dcc38aef14edd4
Romen-Adama-Dev <romen_adama.c.r@outlook.com>
2023-05-14 17:15:44 +0000

    Actualización automática del repositorio, practica 3, 14 of May, 2023

--- script_git.sh
+++ script_git.sh
@@ -4,7 +4,7 @@
 git add .
 
 # Realiza un commit con los cambios
-git commit -m "Actualización automática del repositorio, practica 2, arreglado el apartado anula, falta comentar el código del apartado hitos de la practica 3"
+git commit -m "Actualización automática del repositorio, practica 3, 14 of May, 2023"
 
 # Sube los cambios al repositorio remoto
 git push origin main
