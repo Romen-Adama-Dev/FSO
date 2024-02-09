ac36837e6b7721bd553f51850db0e6ce04851cd2
Romen-Dev <romen_adama.c.r@outlook.com>
2024-02-09 14:01:03 +0000

    Actualización automática del repositorio, practica 2, revision de la practica 2 para segunda entrega

--- script_git.sh
+++ script_git.sh
@@ -6,5 +6,5 @@ git add .
 # Realiza un commit con los cambios
 git commit -m "Actualización automática del repositorio, practica 2, revision de la practica 2 para segunda entrega"
 
-# Sube los cambios al repositorio remoto
-git push origin main
+# Sube los cambios al repositorio remoto en la rama 'master'
+git push origin master
