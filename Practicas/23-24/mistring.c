b5e447da66b18c66ef373e1be0d45e2cc34b3f63
Romen-Dev <romen_Adama.c.r@outlook.com>
2024-02-15 09:11:25 +0000

    Actualización automática del repositorio fecha 2024-02-15 hora 09:11:25 de los archivos .vscode/c_cpp_properties.json "Practicas/23-24/Ficha Practicas/23-24/mistring.c Practicas/23-24/mistring.h Practicas/23-24/test_mistring.c

---
+++ Practicas/23-24/mistring.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+#include "mistring.h"
+
+int mi_strlen (char* str) {
+    int len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    return len;
+}
+
+char* mi_strcpy (char* s1, char* s2) {
+    int i = 0;
+    while ((s1[i] = s2[i]) != '\0') {
+        i++;
+    }
+    return s1;
+}
+
+char* mi_strcat (char* s1, char* s2) {
+    int i = mi_strlen(s1);
+    int j = 0;
+    while ((s1[i++] = s2[j++]) != '\0');
+    return s1;
+}
+
+char* mi_strdup (char* str) {
+    char* dup = malloc((mi_strlen(str) + 1) * sizeof(char));
+    if (dup != NULL) {
+        mi_strcpy(dup, str);
+    }
+    return dup;
+}
+
+int mi_strequals (char* s1, char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return (*s1 - *s2) == 0 ? 1 : 0;
+}
---
+++ Practicas/23-24/mistring.h
@@ -0,0 +1,11 @@
+// mistring.h
+#ifndef MISTRING_H
+#define MISTRING_H
+
+int mi_strlen (char* str);
+char* mi_strcpy (char* s1, char* s2);
+char* mi_strcat (char* s1, char* s2);
+char* mi_strdup (char* str);
+int mi_strequals (char* s1, char* s2);
+
+#endif
